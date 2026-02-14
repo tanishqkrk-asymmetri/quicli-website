93ae06a81dbb5ea4512816beca9b892da9720f54
Tanishq Kaushal <tanishqkrk@gmail.com>
2026-02-14 19:10:43 +0530

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "r.mobirisesite.com"
+        hostname: "r.mobirisesite.com",
       },
       {
         protocol: "https",
-        hostname: "e.mobirise.com"
-      }
-    ]
-  }
+        hostname: "e.mobirise.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
